dee068db6df73c74792e184db7d13bab00f24472
LtCodename <chernenko.eu@gmail.com>
2025-01-19 14:57:27 +0200

    VUE-003 - add vuetify and components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <v-btn color="primary">Hello, Vuetify!</v-btn>
+  <v-app>
+    <v-toolbar app>
+      <v-btn flat to="/">Messages</v-btn>
+      <v-btn flat to="/NewMessage">New Message</v-btn>
+      <v-spacer />
+      <v-btn v-if="!$store.state.token" flat to="/Register">Register</v-btn>
+      <v-btn v-if="!$store.state.token" flat to="/Login">Login</v-btn>
+      <v-btn v-if="$store.state.token" flat @click="$store.commit('logout')"
+        >Logout</v-btn
+      >
+    </v-toolbar>
+
+    <v-content>
+      <br />
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
-<script lang="ts">
+<script>
 export default {
   name: "App",
+  data() {
+    return {};
+  },
 };
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,32 @@
 import { createApp } from "vue";
-
-// Vuetify
-// import "vuetify/styles";
+import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-
-// Components
 import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import MessagesPage from "./components/MessagesPage.vue";
+import NewMessage from "./components/NewMessage.vue";
+import MessagePage from "./components/MessagePage.vue";
+import RegisterPage from "./components/RegisterPage.vue";
+import LoginPage from "./components/LoginPage.vue";
+
+const routes = [
+  { path: "/", component: MessagesPage },
+  { path: "/NewMessage", component: NewMessage },
+  { path: "/Message/:id", component: MessagePage },
+  { path: "/Register", component: RegisterPage },
+  { path: "/LoginPage", component: LoginPage },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL), // Use history mode for clean URLs
+  routes,
+});
 
 const vuetify = createVuetify({
   components,
   directives,
 });
 
-createApp(App).use(vuetify).mount("#app");
+createApp(App).use(vuetify).use(router).mount("#app");
